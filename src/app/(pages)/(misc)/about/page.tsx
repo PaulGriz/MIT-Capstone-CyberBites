ad814a5b67318ebcee4beb1596af75092062ca7c
Paul Griz <paulericgriz@gmail.com>
2023-07-31 15:23:55 -0400

    ADDED: About Page

---
+++ src/app/(pages)/(misc)/about/page.tsx
@@ -0,0 +1,52 @@
+export default function AboutPage() {
+  return (
+    <main className="grid h-full w-full place-items-center">
+      <div className="prose mx-5 mt-8 max-w-[800px] md:prose-lg lg:prose-lg prose-h1:mb-5 md:prose-h1:mb-6 md:prose-p:my-4">
+        <h1 className="text-center">About CyberBites</h1>
+        <p>
+          Greetings, Earthlings, extraterrestrials, and food lovers from across the cosmos! Welcome
+          to &quot;CyberBites,&quot; the ultimate food delivery service for the sci-fi enthusiast
+          and casual diner alike.
+        </p>
+        <p>
+          CyberBites was launched in 2023 during a celestial convergence of creativity and craving.
+          We had a spark of an idea - why should food delivery be confined to the realms of reality?
+          And just like that, CyberBites was uploaded into existence. Our mission? To deliver an
+          ultra-modern, out-of-this-world dining experience that defies space, time, and the
+          boundaries between fiction and reality!
+        </p>
+        <p>
+          With our hyper-speed delivery service, we bring the culinary delights from iconic eateries
+          of your favorite movies and TV shows straight to your front door. Ever dreamed of munching
+          on a slice from the Pizza Planet in Toy Story? Craving a latte from Central Perk in
+          Friends? Or maybe you&apos;d like something more daring from the Mos Eisley Cantina in
+          Star Wars? All this and more are just a click away!
+        </p>
+        <p>
+          Our devoted squad of cybernauts, technomages, and delivery heroes traverse the furthest
+          corners of the galaxy (and your local streets), to ensure that you receive the most
+          interstellar meals, at warp speed. Not only that, we are committed to ensuring that the
+          quality and taste of the food are as authentic as the places they originate from.
+        </p>
+        <p>
+          But CyberBites is more than just food delivery. We&apos;re about fun, laughter, and a
+          sprinkle of healthy eccentricity. We strive to bring a byte of your favorite fictional
+          worlds into your real-life mealtimes, turning every meal into a celebration, an adventure,
+          a delicious memory. At CyberBites, we believe in magic, dreams, and above all, that
+          everyone should have a taste of the cosmos.
+        </p>
+        <p>
+          So join us on this delectable, otherworldly journey across the universe. Strap in, prepare
+          your taste receptors, and get ready for a gastronomic adventure that&apos;s truly beyond
+          this world!
+        </p>
+        <p>
+          What are you waiting for? Place your order now and let us teleport some astounding grub
+          straight to your plate!
+        </p>
+        <p>May the food be with you!</p>
+        <p>CyberBites Team.</p>
+      </div>
+    </main>
+  )
+}
